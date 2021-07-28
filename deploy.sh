e5c019a0c357224b96f6777b0349bb1fbe747a8a
Samuel Gardner <sam.gardner@digital.homeoffice.gov.uk>
2021-07-28 09:55:28 +0100

    HOCS-3380 Parameterising all yamls

--- deploy.sh
+++ deploy.sh
@@ -9,8 +9,6 @@ export VERSION="\"1\""
 
 export DOMAIN="cs"
 
-# export SUBNAMESPACE="${KUBE_NAMESPACE#*-}" # e.g. dev, qa
-
 export MIN_REPLICAS="1"
 export MAX_REPLICAS="1"
 export KUBE_SERVER=https://kube-api-notprod.notprod.acp.homeoffice.gov.uk
@@ -18,17 +16,17 @@ export KC_REALM=https://sso-dev.notprod.homeoffice.gov.uk/auth/realms/hocs-notpr
 export UPTIME_PERIOD="Mon-Fri 08:00-18:00 Europe/London"
 export KUBE_CERTIFICATE_AUTHORITY="https://raw.githubusercontent.com/UKHomeOffice/acp-ca/master/acp-notprod.crt"
 
-# export INTERNAL_DOMAIN="$SUBNAMESPACE-camunda.internal.$DOMAIN-notprod.homeoffice.gov.uk"
+export INTERNAL_DOMAIN="$KUBE_NAMESPACE-camunda.internal.$DOMAIN-notprod.homeoffice.gov.uk"
 
-# echo
-# echo "Deploying hocs-camunda-tools ${VERSION} to ${ENVIRONMENT}"
-# echo "Keycloak realm: ${KC_REALM}"
-# echo "Internal domain: ${INTERNAL_DOMAIN:-nil}"
-# echo
+echo
+echo "Deploying hocs-camunda-tools ${VERSION} to ${KUBE_NAMESPACE}"
+echo "Keycloak realm: ${KC_REALM}"
+echo "Internal domain: ${INTERNAL_DOMAIN:-nil}"
+echo
 
 cd kd || exit 1
 
 kd --timeout 10m \
     -f ingress-internal.yaml \
     -f deployment.yaml \
-    -f service.yaml 
+    -f service.yaml
