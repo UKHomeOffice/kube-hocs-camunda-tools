9be625ca404c5f28eac099393b676f604372726b
Aaron Robinson <aaron.robinson@digital.homeoffice.gov.uk>
2021-08-10 16:30:34 +0100

    Add subnamespace and alter domain

--- deploy.sh
+++ deploy.sh
@@ -5,15 +5,18 @@ export KUBE_NAMESPACE=${ENVIRONMENT:-$DRONE_DEPLOY_TO}
 export KUBE_TOKEN=${KUBE_TOKEN}
 export VERSION=${VERSION}
 
-
 export DOMAIN="cs"
+if [ "${KUBE_NAMESPACE%-*}" == "wcs" ]; then
+    export DOMAIN="wcs"
+fi
 
 export KUBE_SERVER=https://kube-api-notprod.notprod.acp.homeoffice.gov.uk
 export KC_REALM=https://sso-dev.notprod.homeoffice.gov.uk/auth/realms/hocs-notprod
 export UPTIME_PERIOD="Mon-Fri 08:00-18:00 Europe/London"
 export KUBE_CERTIFICATE_AUTHORITY="https://raw.githubusercontent.com/UKHomeOffice/acp-ca/master/acp-notprod.crt"
 
-export INTERNAL_DOMAIN="camunda.${KUBE_NAMESPACE}.internal.${DOMAIN}-notprod.homeoffice.gov.uk"
+export SUBNAMESPACE="${KUBE_NAMESPACE#*-}" # e.g. dev, qa
+export INTERNAL_DOMAIN="camunda.${SUBNAMESPACE}.${DOMAIN}-notprod.homeoffice.gov.uk"
 
 echo
 echo "Deploying hocs-camunda-tools ${VERSION} to ${KUBE_NAMESPACE}"
